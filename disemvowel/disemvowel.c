b0c48587ecd73bf7a2341750723dfd32283c8fd1
mengi024 <mengi024@morris.umn.edu>
2021-09-23 11:32:13 -0500

    implemented first iteration of disemvowel

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,35 @@
 #include <stdlib.h>
+#include <string.h>
+
 
 #include "disemvowel.h"
 
+int isVowel(char a);
+
 char *disemvowel(char *str) {
-  return (char*) "";
+  char result[100];
+  int counter = 0;
+  for(int i = 0; i< strlen(str) ;i++){
+    char current = str[i];
+    if(isVowel(current) == 0){
+      result[counter] = current;
+      counter++;
+    }
+  }
+  result[counter] = '\0';
+  char* final = (char*) calloc(counter + 1, sizeof(char));
+  for(int j = 0; j< strlen(result) - 1 ;j++){
+    final[j] = result[j];
+  }
+  return final;
+}
+
+int isVowel(char a){
+  char *vowels = "aeiouAEIOU";
+  for(int i = 0; i< strlen(vowels);i++){
+    if(a == vowels[i]){
+      return 1;
+    }
+  }
+  return 0;
 }
--- disemvowel/main.c
+++ disemvowel/main.c
@@ -11,6 +11,8 @@ int main(int argc, char *argv[]) {
   line = (char*) malloc (size + 1);
 
   while (getline(&line, &size, stdin) > 0) {
-    printf("%s\n", disemvowel(line));
+    char* result = disemvowel(line);
+    printf("%s\n", result);
+    free(result);
   }
 }
